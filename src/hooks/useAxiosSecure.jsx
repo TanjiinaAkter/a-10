d0a4b2b1049fd00f195cfcbda28692cc29f18edc
tanjiina <tanjina1042@gmail.com>
2025-01-24 23:13:11 +0600

    firebse deploy  done

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuth from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://a-server-coral.vercel.app",
 });
 const useAxiosSecure = () => {
   const { logOut } = useAuth();
